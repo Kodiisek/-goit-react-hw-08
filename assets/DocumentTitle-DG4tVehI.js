cfe023509dac3a99fc476bda81949b3be3335b0f
Kodiisek <Kodiisek@users.noreply.github.com>
2024-12-11 17:53:37 +0000

    deploy: eae76cf85e76eb7e9998db0581debf64336d39ef

--- assets/DocumentTitle-DG4tVehI.js
+++
@@ -1,1 +0,0 @@
-import{j as e,H as s,P as t}from"./index-DY9WgV8Z.js";function i({children:r}){return e.jsx(s,{children:e.jsx("title",{children:r})})}i.propTypes={children:t.node.isRequired};export{i as D};
---
+++ assets/DocumentTitle-DXOyuMSk.js
@@ -0,0 +1,1 @@
+import{j as e,H as s,P as t}from"./index-BXtsm2_J.js";function i({children:r}){return e.jsx(s,{children:e.jsx("title",{children:r})})}i.propTypes={children:t.node.isRequired};export{i as D};
