0906395b11433b2b668c2163c766cded1b73e6cd
Kodiisek <Kodiisek@users.noreply.github.com>
2024-12-14 20:58:52 +0000

    deploy: ec7d85cb1a1aa24f2cd4fcc6174caecb3e7457ad

---
+++ assets/DocumentTitle-DknESgfx.js
@@ -0,0 +1,1 @@
+import{j as e,H as s,P as t}from"./index-DGTopY6F.js";function i({children:r}){return e.jsx(s,{children:e.jsx("title",{children:r})})}i.propTypes={children:t.node.isRequired};export{i as D};
